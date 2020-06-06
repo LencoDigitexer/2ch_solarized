c26f4b08bd80a98a33d98e151ce8650abefc4cb6
Илья Евсеенко <44779327+LencoDigitexer@users.noreply.github.com>
2020-06-06 21:52:11 +0300

    Create 2ch_solarized_dark.js

---
+++ 2ch_solarized_dark.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         2ch solarized dark
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Append solarized theme in 2ch
+// @author       LencoDigitexer
+// @match        http://2ch.hk/*
+// @match        https://2ch.hk/*
+// @grant        none
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+
+addGlobalStyle('body { color: #93a1a1; background: #002b36; }');
+addGlobalStyle('.post_type_reply, .pager, .qr, .bb, .fm__header { background: #073642}');
+addGlobalStyle('.unkfunc{color: #859900;}');
+addGlobalStyle('a, .a-link-emulator {color: #b58900;}');
+addGlobalStyle('.post__reflink {color: #b58900;}');
+addGlobalStyle('.post__anon {color: #b58900;}');
